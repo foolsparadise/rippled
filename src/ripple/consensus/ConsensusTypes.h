0acf0e002639046f5fa3de6ab5ea01a51f0d1615
h111nam <h111nam@h111nams-iMac.local>
2018-03-26 10:35:35 +0800

    rippled Mar 16, 2018 + r Mar 4, 2018 + i none

--- src/ripple/consensus/ConsensusTypes.h
+++ src/ripple/consensus/ConsensusTypes.h
@@ -210,13 +210,13 @@ struct ConsensusResult
     using Dispute_t = DisputedTx<Tx_t, NodeID_t>;
 
     ConsensusResult(TxSet_t&& s, Proposal_t&& p)
-        : set{std::move(s)}, position{std::move(p)}
+        : txns{std::move(s)}, position{std::move(p)}
     {
-        assert(set.id() == position.position());
+        assert(txns.id() == position.position());
     }
 
     //! The set of transactions consensus agrees go in the ledger
-    TxSet_t set;
+    TxSet_t txns;
 
     //! Our proposed position on transactions/close time
     Proposal_t position;
